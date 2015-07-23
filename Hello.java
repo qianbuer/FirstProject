9a3a300f63634aedd01d1f8db444b29804621c37
qianbuer <53428399@163.com>
2015-07-24 01:02:07 +0800

    Revert "Revert "abc""
    This reverts commit 874d12fb583c219173e9e93a8bac4c5aaad1224c.

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 public Hello {
 	public static void main(String[] args) {
 		System.out.println("the first project on github!");
-		System.out.println("hahaha");
-		System.out.println("end");
-		System.out.println("test branch");
+
 	}
 }
